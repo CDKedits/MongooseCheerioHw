5bbcd62d706b6e0b53cf8900c1cdc0c2ec68694d
C.Nguyen <cdkedits@gmail.com>
2019-07-14 20:44:58 -0700

    initialized React app

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = _ => {
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const app = express()
 const { join } = require(`path`)
 
 app.use(express.static(join(__dirname, '/public')))
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
-app.listen(3000)
+mongoose.connect(`mongodb:localhost/news_db`)
+  .then(_ => app.listen(3001))
+  .catch(e => console.log(e))
